90455abaeacf1ebb14bd275dae2050253ec8710b
Ankush523 <ankush.ad050203@gmail.com>
2023-04-24 07:50:17 +0530

    straker integrated

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -4,6 +4,7 @@ import List from './List'
 import Image from 'next/image'
 import graph from './graph.png'
 import wallet from './wallet.png'
+import Link from 'next/link'
 const Homepage = () => {
   return (
     <div className='flex flex-col'>
@@ -30,7 +31,7 @@ const Homepage = () => {
 
             <div className='flex flex-col bg-white1 text-black1 rounded-2xl p-8 w-[450px] shadow-xl'>
                 <label className='text-2xl'>Total Income</label>
-                <label className='text-sm mb-[10px] text-silver'>1 January - 31 January 2023</label>
+                <label className='text-sm mb-[10px] text-silver'>1 April - 30 April 2023</label>
                 <hr/>
                 <div className='flex flex-row justify-between'>
                     <div className='flex flex-col'>
@@ -42,14 +43,11 @@ const Homepage = () => {
             </div>
 
             <div className='flex flex-col bg-white1 text-black1 rounded-2xl p-8 w-[450px] shadow-xl'>
-                <label className='text-2xl'>Total Expends</label>
-                <label className='text-sm mb-[10px] text-silver'>1 January - 31 January 2023</label>
+                <label className='text-2xl'>Subscriptions</label>
+                <label className='text-sm mb-[10px] text-silver'>1 April - 30 April 2023</label>
                 <hr/>
                 <div className='flex flex-row justify-between'>
-                    <div className='flex flex-col'>
-                        <label className='text-2xl font-semibold mt-[10px]'>$127,000</label>
-                        <label className='text-red1'>-13% last year</label>
-                    </div>
+                    <button className='text-blue1 bg-blue1/10 px-[20px] my-[15px] rounded-lg'>Continue to Subs. page</button>
                     <Image src={graph} alt="graph" className='h-[80px] w-[150px]'/>
                 </div>
             </div>
@@ -58,9 +56,12 @@ const Homepage = () => {
         <div className='flex flex-row space-x-[50px] px-[100px] bg-white2 pb-[20px]'>
 
             <div className='flex flex-col bg-white1 text-black1 rounded-2xl p-8 w-[550px] shadow-2xl'>
-                <label className='text-2xl'>Revenue Transaction</label>
-                <label className='text-sm mb-[10px] text-silver'>Latest transations all the time</label>
+                <label className='text-2xl'>Flow DEX</label>
+                <label className='text-sm mb-[10px] text-silver'>Loans, Wrap/Unwrap, Faucet and more</label>
                 <hr/>
+                <p className='mt-[15px] text-lg'>Realtime staking platform that allows you to stake your assets in the POLYGON chain.</p>
+                <p className='text-lg'>Get staking rewards directly streamed to your wallet per second</p>
+                <button className='mt-[20px] text-blue1 bg-blue1/10 py-[5px] rounded-lg'><Link href="https://straker-mocha.vercel.app/">Proceed to DEX</Link></button>
             </div>
 
             <div className='flex flex-col bg-white1 text-black1 rounded-2xl p-8 w-[850px] shadow-2xl'>
